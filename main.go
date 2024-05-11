a6e9f7ff7d55c569c55cf61b14b302177dd1775e
Yong Yu <ypyu@hotmail.com>
2024-05-10 17:31:12 -0700

    modified main.go to use mux

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
+	rtr := mux.NewRouter()
+
+	rtr.HandleFunc("/{topic}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		w.Write([]byte("Topic: " + vars["topic"]))
 	})
+	http.Handle("/", rtr)
 
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
